fc39851fd2297a96e07650f2e05f15577f383bc7
dongree <dongwoo0307@naver.com>
2023-04-26 23:57:02 +0900

    update home page (overflow scroll)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,10 +48,19 @@ export default function Home() {
       description:
         'Product Maestro에 새롭게 추가된 기능들이에요\n프로필에 Role을 표시할 수 있도록 바뀌었어요!\n관심있는 프로덕트를 구독할 수 있어요!\n 하하',
     },
+    {
+      projectname: 'Cool Project',
+      username: '김승빈',
+      teamname: 'teamname',
+      date: '23.04.28 09:20',
+      title: '[Update] 새로운 기능을 추가했어요',
+      description:
+        'Product Maestro에 새롭게 추가된 기능들이에요\n프로필에 Role을 표시할 수 있도록 바뀌었어요!\n관심있는 프로덕트를 구독할 수 있어요!\n 하하',
+    },
   ];
 
   return (
-    <section className="basis-4/5 bg-gray-100 px-28 py-10">
+    <section className="basis-4/5 bg-gray-100 px-28 py-10 flex flex-col justify-center">
       {/* 구독까지 구현 가능할까..? */}
       {/* <p className="text-2xl font-bold ">내가 구독하는 프로덕트</p>
       <ul className="flex my-7">
@@ -74,7 +83,7 @@ export default function Home() {
           <p className="text-xs font-bold my-2">Product Maestro</p>
         </li>
       </ul> */}
-      <ul className="flex flex-col">
+      <ul className="flex flex-col overflow-scroll h-5/6 overflow-x-hidden">
         {data.map(item => (
           <Card info={item} />
         ))}
